bdb27293f2ddcf2f1a6bf2d66162fab5aa2488c3
Kusmiczki Balázs <kusmiczkibalazs@gmail.com>
2021-11-06 20:55:47 +0100

    Added new commands to the configuration class

--- ticket-service/src/main/java/com/epam/training/ticketservice/ui/configuration/ApplicationConfiguration.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/ui/configuration/ApplicationConfiguration.java
@@ -1,6 +1,17 @@
 package com.epam.training.ticketservice.ui.configuration;
 
-import com.epam.training.ticketservice.ui.command.impl.AbstractCommand;
+import com.epam.training.ticketservice.ui.command.impl.*;
+import com.epam.training.ticketservice.ui.command.impl.moviecommands.CreateMovieCommand;
+import com.epam.training.ticketservice.ui.command.impl.moviecommands.DeleteMovieCommand;
+import com.epam.training.ticketservice.ui.command.impl.moviecommands.ListMoviesCommand;
+import com.epam.training.ticketservice.ui.command.impl.moviecommands.UpdateMovieCommand;
+import com.epam.training.ticketservice.ui.command.impl.roomcommands.CreateRoomCommand;
+import com.epam.training.ticketservice.ui.command.impl.roomcommands.DeleteRoomCommand;
+import com.epam.training.ticketservice.ui.command.impl.roomcommands.ListRoomsCommand;
+import com.epam.training.ticketservice.ui.command.impl.roomcommands.UpdateRoomCommand;
+import com.epam.training.ticketservice.ui.command.impl.screeningcommands.CreateScreeningCommand;
+import com.epam.training.ticketservice.ui.command.impl.screeningcommands.DeleteScreeningCommand;
+import com.epam.training.ticketservice.ui.command.impl.screeningcommands.ListScreeningsCommand;
 import com.epam.training.ticketservice.ui.interpreter.CommandLineInterpreter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +24,60 @@ public class ApplicationConfiguration {
     @Bean
     public CommandLineInterpreter commandLineInterpreter(Set<AbstractCommand> abstractCommands) {
         return new CommandLineInterpreter(System.in, System.out, abstractCommands);
+    }
+
+    @Bean
+    public AbstractCommand createMovieCommand () {
+        return new CreateMovieCommand();
+    }
+
+    @Bean
+    public AbstractCommand updateMovieCommand () {
+        return new UpdateMovieCommand();
+    }
+
+    @Bean
+    public AbstractCommand deleteMovieCommand () {
+        return new DeleteMovieCommand();
+    }
+
+    @Bean
+    public AbstractCommand listMoviesCommand () {
+        return new ListMoviesCommand();
+    }
+
+    @Bean
+    public AbstractCommand createRoomCommand () {
+        return new CreateRoomCommand();
+    }
+
+    @Bean
+    public AbstractCommand updateRoomCommand () {
+        return new UpdateRoomCommand();
+    }
+
+    @Bean
+    public AbstractCommand deleteRoomCommand () {
+        return new DeleteRoomCommand();
+    }
+
+    @Bean
+    public AbstractCommand listRoomsCommand () {
+        return new ListRoomsCommand();
+    }
+
+    @Bean
+    public AbstractCommand createScreeningCommand () {
+        return new CreateScreeningCommand();
+    }
+
+    @Bean
+    public AbstractCommand deleteScreeningCommand () {
+        return new DeleteScreeningCommand();
+    }
+
+    @Bean
+    public AbstractCommand listScreeningsCommand () {
+        return new ListScreeningsCommand();
     }
 }
