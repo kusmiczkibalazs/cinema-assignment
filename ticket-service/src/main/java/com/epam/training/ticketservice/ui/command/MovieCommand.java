57782cbcc02a5593506d6df9b2e7332d47fd4a7f
Kusmiczki Balázs <kusmiczkibalazs@gmail.com>
2021-11-18 23:37:14 +0100

    Connected commands with the model

--- ticket-service/src/main/java/com/epam/training/ticketservice/ui/command/MovieCommand.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/ui/command/MovieCommand.java
@@ -1,28 +1,45 @@
 package com.epam.training.ticketservice.ui.command;
 
+import com.epam.training.ticketservice.core.movie.MovieService;
+import com.epam.training.ticketservice.core.movie.model.MovieDto;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+
+import java.util.List;
 
 @ShellComponent
 public class MovieCommand {
 
+    private final MovieService movieService;
+
+    public MovieCommand(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
     @ShellMethod(key = "create movie", value = "Create a new movie")
-    public String createMovie(String title, String genre, int lengthInMinutes) {
-        return "%%% Created movie: " + title + ", genre: " + genre + ", length: " + lengthInMinutes + " minutes";
+    public MovieDto createMovie(String title, String genre, int lengthInMinutes) {
+        MovieDto movieDto = MovieDto.builder()
+                .title(title)
+                .genre(genre)
+                .lengthInMinutes(lengthInMinutes)
+                .build();
+        movieService.createMovie(movieDto);
+        return movieDto;
     }
 
     @ShellMethod(key = "update movie", value = "Update a movie")
     public String updateMovie(String title, String genre, int lengthInMinutes) {
+        //TODO
         return "%%% Updated movie: " + title + ", genre: " + genre + ", length: " + lengthInMinutes + " minutes";
     }
 
     @ShellMethod(key = "delete movie", value = "Delete a movie")
-    public String deleteMovie(String title) {
-        return "%%% Deleted movie: " + title;
+    public void deleteMovie(String title) {
+        movieService.deleteMovie(title);
     }
 
     @ShellMethod(key = "list movies", value = "List the movies")
-    public String listMovies() {
-        return "%%% An imaginary list of movies";
+    public List<MovieDto> listMovies() {
+        return movieService.getMovieList();
     }
 }
